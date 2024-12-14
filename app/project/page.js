bf7b2928e3fc776691eb2fffea08b1a25ab7ae97
Tarun Gupta <115605648+TarunGupta2602@users.noreply.github.com>
2024-12-14 14:29:02 +0530

    Update page.js

--- app/project/page.js
+++ app/project/page.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Image from 'next/image'
 import projetImage from '@/public/Screenshot 2024-10-20 153643.png'
+import projetImage1 from '@/public/Screenshot_20241214_142522.png'
+
 
 const Page = () => {
   return (
@@ -16,6 +18,24 @@ const Page = () => {
         {/* Button that appears on hover */}
         <a 
           href="https://tarungupta2602.github.io/webtask.github.io/" 
+          className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-60 opacity-0 group-hover:opacity-100 transition-opacity duration-300 rounded-lg"
+        >
+          <button className="px-4 py-2 bg-blue-600 text-white font-semibold rounded hover:bg-blue-700">
+            View Project
+          </button>
+        </a>
+      </div>
+             <div className="relative group w-80 h-52 md:w-96 md:h-60"> {/* Adjusted width and height */}
+        <Image 
+          src={projetImage1} 
+          className="rounded-lg object-cover" 
+          alt="Project2"
+          layout="fill" // Ensures the image covers the entire container
+        />
+        
+        {/* Button that appears on hover */}
+        <a 
+          href="https://webyug.onrender.com" 
           className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-60 opacity-0 group-hover:opacity-100 transition-opacity duration-300 rounded-lg"
         >
           <button className="px-4 py-2 bg-blue-600 text-white font-semibold rounded hover:bg-blue-700">
